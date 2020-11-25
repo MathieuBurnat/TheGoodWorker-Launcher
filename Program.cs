4b3eec7f5f1ac36c5ea411cea0f226fa63a2b869
MathieuBurnat <70205357+MathieuBurnat@users.noreply.github.com>
2020-11-25 16:27:38 +0100

    Beta 0.9 (#1)
    ## Goals
    - [x] The program is able to :
        - [x] Execute wished application's list
        - [x] Browse wished url's list
    - [ ] Command line program
        - [ ] Primitif code color 
            - Example : Errors in red, nice info in blue / green
    - [x] Configuration file
        - [x] It's possible to manually add a new program
            - Example : - Discord : **C:/.../Discord.exe**
        - [x] It's possible to manually add URL that'll be execute into a browser
            - Example : myurl : **wwww.myUrl.com**
        - [x] Option : Automatically-close
            - If is set on true, close the windows
    - [ ] Easy to install
        - [ ] Get Started

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Diagnostics;
 
 namespace TheGoodWorker_Launcher
 {
@@ -6,8 +10,56 @@ namespace TheGoodWorker_Launcher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //Get hastable lists
+            Hashtable configuration = (Hashtable)ConfigurationManager.GetSection("items/applicationConfiguration");
+            Hashtable applicationList = (Hashtable)ConfigurationManager.GetSection("items/applications");
+            Hashtable urlList = (Hashtable)ConfigurationManager.GetSection("items/urls");
+
+            //Set configuration varaibles
+            bool should_automatically_close = StringToBool(configuration["should_automatically_close"].ToString());
+            string browser_path = configuration["browser_path"].ToString();
+
+            //Display the application list
+            Console.WriteLine("Display application list");
+            foreach (DictionaryEntry a in applicationList)
+            {
+                Console.WriteLine("{0} : {1}", a.Key, a.Value);
+                // -> Execute the application
+                Console.WriteLine("Launching {0}...", a.Key);
+                var p = new Process();
+                p.StartInfo = new ProcessStartInfo((string)a.Value)
+                {
+                    UseShellExecute = true
+                };
+                p.Start();
+            }
+
+            //Display the application list
+            Console.WriteLine("Display url list");
+            foreach (DictionaryEntry u in urlList)
+            {
+                Console.WriteLine("{0} : {1}", u.Key, u.Value);
+                // -> Browse the url
+                Console.WriteLine("Launching {0}...", u.Key);
+
+                Process.Start(browser_path, (string)u.Value);
+            }
+
+            Console.WriteLine(configuration["should_automatically_close"]);
+            Console.WriteLine("Done ! :D");
+
+            if(!should_automatically_close)
+                Console.ReadKey();
+        }
+
+        private static bool StringToBool(string strB)
+        {
+            bool b;
+            if (bool.TryParse(strB, out b))
+                return b;
+
+            Console.WriteLine("[Error] Impossible to parse the value " + strB + ". You should to check the App.Config ! The variable 'should_automatically_close' is set to false in the meantime."); 
+            return false;
         }
     }
-}
+}             
---
+++ README.md
@@ -0,0 +1,30 @@
+# TheGoodWorker-Launcher
+## Description
+I have to open a bunch of programs and urls **each morning** at my teleworking days.
+So, i want a simple command-line program that lunch my differents things when i execute it.
+
+## Goals
+- [x] The program is able to :
+    - [x] Execute wished application's list
+    - [x] Browse wished url's list
+- [ ] Command line program
+    - [ ] Primitif code color 
+        - Example : Errors in red, nice info in blue / green
+- [x] Configuration file
+    - [x] It's possible to manually add a new program
+        - Example : - Discord : **C:/.../Discord.exe**
+    - [x] It's possible to manually add URL that'll be execute into a browser
+        - Example : myurl : **wwww.myUrl.com**
+    - [x] Option : Automatically-close
+        - If is set on true, close the windows
+- [ ] Easy to install
+    - [ ] Get Started
+
+## Get Started (future)
+- Download the last version of the .exe,
+- Add your bunch of applications / urls into the *Application.config* while following the examples.
+- Execute the programm
+- Have fun !
+
+## Author
+Created by Mathieu Burnat ! :D
